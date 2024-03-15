35e966e60f1a3f73f9c7b667bcfc82e323e9cf0d
Shakeel Sheikh <162777210+SHAKEELCDACKH@users.noreply.github.com>
2024-03-15 12:20:11 +0530

    Add files via upload

--- ArithmeticConversions.java
+++ ArithmeticConversions.java
@@ -1,10 +1,20 @@
 public class ArithmeticConversions {
     public static void main(String[] args) {
-        int num1 = 10;
-        double num2 = 5.2; 
+        int num1 = 15;
+        double num2 = 8.5;
+        float num3 = 3.7f;
 
-        double result = num1 + num2; 
+        double result1 = num1 + num2;
+        System.out.println("int + double = double: " + result1);
 
-        System.out.println("Integer + double = double: " + result); 
+        float result2 = num1 - num3;
+        System.out.println("int - float = float: " + result2);
+
+        double result3 = num3 * num2;
+        System.out.println("float * double = double: " + result3); 
+
+        double result4 = num1 / num2;
+        System.out.println("int / double = double: " + result4);
+
     }
 }
